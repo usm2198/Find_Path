f08cb189468fb5ed70980333adf754eb09be159b
usm2198 <101632664+usm2198@users.noreply.github.com>
2022-08-28 10:45:17 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,23 @@
+# findPath
+LeetCode challenge [findPath](https://leetcode.com/playground/LhwrauHo)
+
+- Write method findPath
+- Should take two params:
+  - object
+  - keys separated by dots as string
+- Return value if it exists at that path inside the object, else return undefined
+
+## Algorithm
+
+1. Start the program
+2. create a function findPath that takes 2 arguments
+   - 2.1 Object as first argument and path as second
+3. Split the path using split('.') function with .(dot) as parameter and store in a variable as an array
+4. Use a for..of loop to iterate over array variable from step 3 (say pathArray)
+    - 4.1 Assign value of obj[pathArray] to obj
+      - 4.1.1 if step 4.1 value is undefined, assign obj to undefined and break the loop, else goto 4.1.2
+      - 4.1.2 else let loop run
+5. End for..of loop
+6. Return obj
+
+  
---
+++ findPath.js
@@ -0,0 +1,42 @@
+/*
+- Write method findPath
+- Should take two params:
+    - object
+    - keys separated by dots as string
+- Return value if it exists at that path inside the object, else return undefined
+*/
+
+function findPath(obj,path){
+    var pathArray = path.split('.');
+    // console.log(pathArray);
+    for(var i of pathArray){
+        obj = obj[i];
+        if(obj === undefined){
+            break;
+        }
+    }
+    return obj;
+};
+
+var obj = {
+    a: {
+        b: {
+            c: 12,
+            j: false
+        },
+        k: null
+    }
+};
+
+
+
+console.log(findPath(obj, 'a.b.c')); // 12
+console.log(findPath(obj, 'a.b')); // {c: 12, j: false}
+console.log(findPath(obj, 'a.b.d')); // undefined
+console.log(findPath(obj, 'a.c')); // undefined
+console.log(findPath(obj, 'a.b.c.d')); // undefined
+console.log(findPath(obj, 'a.b.c.d.e')); // undefined
+console.log(findPath(obj, 'a.b.j')); //false
+console.log(findPath(obj, 'a.b.j.k')); //undefined
+console.log(findPath(obj, 'a.k')); //null
+
